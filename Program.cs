69ecdc2b15aa770f3cd75af46e5c9933c6e77c04
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-17 17:02:52 +0100

    exercice non corrigé

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ABAuCarre, BC, CA;
+            Console.WriteLine("Quelle est la longueur de BC ?");
+            BC = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quelle est la longueur de CA ?");
+            CA = double.Parse(Console.ReadLine());
+
+            ABAuCarre = Math.Pow(BC, 2) + Math.Pow(CA, 2);
+
+            Console.WriteLine($"AB est égal à {Math.Sqrt(ABAuCarre)}");
+
+        }
+    }
+}
